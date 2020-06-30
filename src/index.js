709ec8e1ca85fa1dd6ebf59065d2526b35fc0fc4
JohnNils Olson <johnnils@gmail.com>
2020-06-30 12:01:29 -0700

    add dynamic status display for mulitple tamagotchis
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { Doritama } from './../src/doritama.js';
 import { Tamagotchi } from './../src/tamagotchi.js';
 
 $(document).ready(function() {
-  
   let doritama;
   let tamagotchi;
 
@@ -25,45 +24,28 @@ $(document).ready(function() {
     tamagotchi.setSleep();
     doritama.tamagotchis.push(tamagotchi);
 
-    $("#tamagotchi-name").text(tamagotchi.name);
-    $("#tamagotchis").append(`<button type="button" class="btn btn-primary">${tamagotchi.name}</button>`)
-
-    setInterval(function() {
-      if (tamagotchi.food > 7) {
-        $("#hunger-level").text("Satisfied!");
-      } else if (tamagotchi.food > 3) {
-        $("#hunger-level").text("Hungry!");
-      } else {
-        $("#hunger-level").text("Starving!");
-      }
-      if (tamagotchi.play > 7) {
-        $("#play-level").text("Content!");
-      } else if (tamagotchi.play > 3) {
-        $("#play-level").text("Bored!");
-      } else {
-        $("#play-level").text("Wearied!");
-      }
-      if (tamagotchi.sleep > 7) {
-        $("#sleep-level").text("Rested!");
-      } else if (tamagotchi.sleep > 3) {
-        $("#sleep-level").text("Tired!");
-      } else {
-        $("#sleep-level").text("Exhausted!");
-      }      
-    }, 1000);
+    $("#tamagotchis").append(`<button type="button" class="btn btn-primary" id="${tamagotchi.name}">${tamagotchi.name}</button>`)
+    $("body").append(`<div><h1 id="name-${tamagotchi.name}">${tamagotchi.name}</h1><h6>Tamagotchi</h6></div><div><h1 class="hunger-level" id="hunger-${tamagotchi.name}">${tamagotchi.foodStatus}</h1><h6>Hunger Level</h6><button type="button" class="btn btn-primary" id="feed">Feed your Tamagotchi!</button></div><div><h1 id="play-${tamagotchi.name}">${tamagotchi.playStatus}</h1><h6>Play Level</h6><button type="button" class="btn btn-primary" id="play">Play with your Tamagotchi!</button></div><div><h1 id="sleep-${tamagotchi.name}">${tamagotchi.sleepStatus}</h1><h6>Sleep Level</h6><button type="button" class="btn btn-primary" id="sleep">Put your Tamagotchi to bed!</button></div>`);
 
     setInterval(function() {
       if (tamagotchi.starved === true || tamagotchi.neglected === true || tamagotchi.exhausted === true) {
         $("#tamagotchi-info").hide();
-        $("#death").show();
+        $("#death").append(`<h1>Your Tamagotchi has died!</h1>`);
       }
       tamagotchi.isStarved();
       tamagotchi.isNeglected();
       tamagotchi.isExhausted();
     }, 1000);
   
-    $("#tamagotchi-info").show();
+//    $("#tamagotchi-info").show();
   });
+
+  setInterval(function(){
+    tamagotchi.displayStatus();
+    $(`#hunger-${tamagotchi.name}`).text(tamagotchi.foodStatus);
+    $(`#play-${tamagotchi.name}`).text(tamagotchi.playStatus);
+    $(`#sleep-${tamagotchi.name}`).text(tamagotchi.sleepStatus);
+  }, 1000);
 
   $("#feed").click(function() {
     tamagotchi.feed();
@@ -80,4 +62,16 @@ $(document).ready(function() {
   $("#refresh").click(function(){
     location.reload(true);
   });
+
+  // $(`#${tamagotchi.name}`).click(function() {
+
+  //   $("#tamagotchi-name").text(tamagotchi.name);
+
+  //   setInterval(function(){
+  //     tamagotchi.displayStatus();
+  //     $("#hunger-level").text(tamagotchi.foodStatus);
+  //     $("#play-level").text(tamagotchi.playStatus);
+  //     $("#sleep-level").text(tamagotchi.sleepStatus);
+  //   }, 1000);
+  // });
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -5,12 +5,18 @@ export class Tamagotchi {
     this.food = 10;
     this.play = 10;
     this.sleep = 10;
+    this.foodStatus = "";
+    this.playStatus = "";
+    this.sleepStatus = "";
   }
 
   setHunger() {
     setInterval(() => {
       this.food--;
     }, 1000);
+    if (this.food === 0) {
+      clearInterval()
+    }
   }
 
   setPlay() {
@@ -37,27 +43,53 @@ export class Tamagotchi {
     this.sleep = 10;
   }
 
-    isStarved() {
-      if (this.food > 0) {
-        return this.starved = false;
-      } else {
-        return this.starved = true;
-      }
+  isStarved() {
+    if (this.food > 0) {
+      return this.starved = false;
+    } else {
+      return this.starved = true;
     }
+  }
 
-    isNeglected() {
-      if (this.play > 0) {
-        return this.neglected = false;
-      } else {
-        return this.neglected = true;
-      }
+  isNeglected() {
+    if (this.play > 0) {
+      return this.neglected = false;
+    } else {
+      return this.neglected = true;
     }
+  }
 
-    isExhausted() {
-      if (this.sleep > 0) {
-        return this.exhausted = false;
-      } else {
-        return this.exhausted = true;
-      }
+  isExhausted() {
+    if (this.sleep > 0) {
+      return this.exhausted = false;
+    } else {
+      return this.exhausted = true;
     }
+  }
+
+  displayStatus() {
+      if (this.food > 7) {
+        this.foodStatus = "Satisfied!";
+      } else if (this.food > 3) {
+        this.foodStatus = "Hungry!";
+      } else {
+        this.foodStatus = "Starving!";
+      }
+      if (this.play > 7) {
+        this.playStatus = "Content!";
+      } else if (this.play > 3) {
+        this.playStatus = "Bored!";
+      } else {
+        this.playStatus = "Wearied!";
+      }
+      if (this.sleep > 7) {
+        this.sleepStatus = "Rested!";
+      } else if (this.sleep > 3) {
+        this.sleepStatus = "Tired!";
+      } else {
+        this.sleepStatus = "Exhausted!";
+      }      
+  }
+
+
 }
